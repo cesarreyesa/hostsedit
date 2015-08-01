2b3b16df5a04ae91d49caa4eaa46fb0682b3633f
Cesar Reyes <cesarreyesa@gmail.com>
2015-08-01 12:50:41 -0500

    Se agrega una dependencia, hostile, este modulo se encarga de manejar el archivo hosts. Se muestra la lista de entradas

--- edit.js
+++ edit.js
@@ -1,15 +1,12 @@
+
 angular.module('editApp', [])
   .controller('EditController', function() {
     var edit = this;
-    var fs = require('fs');
-    var isWin = /^win/.test(process.platform);
-    var path = '';
-    if(isWin){
-      path = 'C:\\Windows\\System32\\drivers\\etc\\hosts';
-    }else{
-      path = '/etc/hosts'
-    }
-    edit.fileText = fs.readFileSync(path, {encoding: 'utf8'});
-    console.log(edit.fileText);
-//    edit.fileText = fileText;
+    var hostile = require('hostile')
+    edit.lines = []
+    var lines = hostile.get(false);
+    lines.forEach(function (line) {
+      edit.lines.push(line);
+    })
+    console.log(edit.lines);
   });
