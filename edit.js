6bd529918f5e98fa0c12e52851f0627b131142ae
Cesar Reyes <cesarreyesa@gmail.com>
2015-08-15 13:19:09 -0500

    Ya agrega registros, aunque no los muestra todavía.

--- edit.js
+++ edit.js
@@ -6,7 +6,16 @@ angular.module('editApp', [])
     edit.lines = []
     var lines = hostile.get(false);
     lines.forEach(function (line) {
-      edit.lines.push(line);
-    })
-    console.log(edit.lines);
+      edit.lines.push({ip: line[0], host: line[1]});
+    });
+    edit.entry = {};
+    edit.addRecord = function() {
+      if(hostile.set(edit.entry.ip, edit.entry.host)){
+        edit.lines.push({ip:edit.entry.ip, host:edit.entry.host});
+        edit.entry.ip = '';
+        edit.entry.host = '';
+      }else{
+        console.log("error");
+      }
+    };
   });
